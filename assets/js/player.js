cbe1669608cec85fe139448c8b3ee6324eec8ca4
Jack Biggin <me@jackbiggin.me>
2018-03-23 21:11:01 +0000

    Implemented pause/go live functionality with timer

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,19 +1,25 @@
-$(document).ready(function() {
-    stream = new Howl({
-        src: ['https://streamer.radio.co/s986435880/listen'],
-        format: 'mp3',
-        html5: true
-    });
-
+/* Set variables */
+var stream = new Howl({
+    src: ['https://streamer.radio.co/s986435880/listen'],
+    format: 'mp3',
+    html5: true
 });
 
+pausedMins = 0;
+pausedSecs = 0;
+
+pauseTimerUpdate = setInterval(updatePausedTimer, 1000);
+
+/* Ran when play/pause button clicked */
 function togglePlayback(isPlaying) {
     if(isPlaying) {
         stream.pause();
         changePlaybackStatus(false);
+        togglePausedTimer(true);
     } else {
         stream.play();
         changePlaybackStatus(true);
+        togglePausedTimer(false);
     }
 }
 
@@ -27,4 +33,39 @@ function changePlaybackStatus(newStatus) {
     } else {
         document.getElementById("play-button").innerHTML = '<i class="fas fa-play"></i>';
     }
+}
+
+/* Handles timer that shows up when paused */
+function togglePausedTimer(isPaused) {
+    if(isPaused) {
+        pausedMins = 0;
+        pausedSecs = 0;
+        document.getElementById("paused-timer").innerHTML = "0:00";
+        document.getElementById("paused-data-container").style.display = "block";
+    } else {
+        document.getElementById("paused-data-container").style.display = "none";
+    }
+}
+
+function updatePausedTimer() {
+    pausedSecs++;
+
+    if(pausedSecs == 60) {
+        pausedSecs = 0;
+        pausedMins++;
+    }
+
+    if(pausedSecs < 10) { // Always show seconds as 2 digits - there's probably a better way to do this
+        displayPausedSecs = "0" + pausedSecs;
+    } else {
+        displayPausedSecs = pausedSecs;
+    }
+
+    document.getElementById("paused-timer").innerHTML = pausedMins + ":" + displayPausedSecs;
+}
+
+/* Return live functionality */
+function goLive() {
+    stream.unload();
+    togglePlayback(false);
 }
